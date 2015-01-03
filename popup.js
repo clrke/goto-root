17c71bad8c662c0d54daf61462fa183329c5d316
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-01-03 21:07:35 +0800

    Print homepage of website to popup

--- popup.js
+++ popup.js
@@ -2,4 +2,5 @@ chrome.tabs.query({'active': true}, function(tabs) {
   var urlSplit = tabs[0].url.split("/");
   var newUrl = urlSplit[0] + "//" + urlSplit[2]
   chrome.tabs.update(tabs[0].id, {url: newUrl});
+  document.write(newUrl);
 });
